18d32be29ce15412f522c38c7acf8c8d96a816a2
Regina-Ibragimova <ibragimovarn@yandex.ru>
2022-06-13 12:58:08 +0500

    flake8

--- api_yamdb/api/urls.py
+++ api_yamdb/api/urls.py
@@ -1,8 +1,13 @@
+from api.views import (
+    CategoryViewSet,
+    CommentViewSet,
+    GenreViewSet,
+    ReviewViewSet,
+    TitleViewSet,
+    UserViewSet,
+)
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-
-from api.views import (CategoryViewSet, CommentViewSet, GenreViewSet,
-                       ReviewViewSet, TitleViewSet, UserViewSet)
 
 app_name = 'api'
 

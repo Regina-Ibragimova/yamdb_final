5597360da64a08cabd9c2357c493054e55bfe4f1
Regina-Ibragimova <ibragimovarn@yandex.ru>
2022-06-17 10:55:29 +0500

    deploy telega

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -59,6 +59,17 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'api_yamdb.wsgi.application'
 
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv('DB_ENGINE', default='django.db.backends.postgresql'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT'),
+    }
+}
+
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': f'django.contrib.auth.password_validation.{name}'}
     for name in [
